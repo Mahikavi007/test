9a2b5bff1dc3b9939d2efc8df5fdff0edbd9d2f8
Mahesh <mahesh.gv@evaphotofilms.com>
2024-06-05 02:41:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,16 @@
-pip install streamlit msal
+def install_and_import(package):
+    import importlib
+    try:
+        importlib.import_module(package)
+    except ImportError:
+        import pip
+        pip.main(['install', package])
+    finally:
+        globals()[package] = importlib.import_module(package)
+
+
+install_and_import('msal')
+
 import streamlit as st
 import pandas as pd
 import requests
